5ef99b13152ec72e03938266e7a705cfe2413adf
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-01-17 16:13:55 +0100

    Update export

--- src/evr/export_ontology.py
+++ src/evr/export_ontology.py
@@ -1,14 +1,19 @@
 """A data model and export scripts for EVR."""
 
+from __future__ import annotations
+
 from pathlib import Path
 
 import click
-from tqdm import tqdm
+from pyobo import Reference, Term, TypeDef
+from pyobo.ssg import make_site
+from pyobo.struct import make_ad_hoc_ontology
+from pyobo.struct.typedef import exact_match
 
+from evr import Venue
 from evr.model import load_venues
 
 __all__ = [
-    "export_ontology",
     "main",
 ]
 
@@ -16,77 +21,100 @@ HERE = Path(__file__).parent.resolve()
 ROOT = HERE.parent.parent.resolve()
 OUTPUT = ROOT.joinpath("output")
 OUTPUT.mkdir(exist_ok=True)
+ONTOLOGY_OBO_PATH = OUTPUT.joinpath("venues.obo")
 ONTOLOGY_TTL_PATH = OUTPUT.joinpath("venues.ttl")
 ONTOLOGY_OWL_PATH = OUTPUT.joinpath("venues.owl")
 
-PREFIX = "EVR"
+HTML_DIRECTORY = ROOT.joinpath("docs")
+
+PREFIX = "evr"
 BASE_IRI = "https://w3id.org/evr/venue/"
 ONTOLOGY_IRI = "https://w3id.org/evr/evr.ttl"
 
-PREAMBLE = f"""\
-@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-@prefix terms: <http://purl.org/dc/terms/> .
-@prefix owl: <http://www.w3.org/2002/07/owl#> .
-@prefix oboInOwl: <http://www.geneontology.org/formats/oboInOwl#> .
-@prefix wikidata: <http://www.wikidata.org/entity/> .
-@prefix RO: <http://purl.obolibrary.org/obo/RO_> .
-@prefix IAO: <http://purl.obolibrary.org/obo/IAO_> .
-@prefix orcid: <https://orcid.org/> .
-@prefix geonames: <https://www.geonames.org/> .
-@prefix schema: <https://schema.org/> .
-@prefix osmw: <https://www.openstreetmap.org/way/> .
-@prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> .
+CONFERENCE_VENUE_TERM = Term.from_triple("ENVO", "03501127", name="conference venue")
+CONFERENCE_CITY_TERM = Term.from_triple("ENVO", "00000856", name="city")
 
-@prefix evr: <{BASE_IRI}> .
-@prefix venue: <http://purl.obolibrary.org/obo/ENVO_03501127> .
-@prefix city: <http://purl.obolibrary.org/obo/ENVO_00000856> .
-@prefix human: <http://purl.obolibrary.org/obo/NCBITaxon_9606> .
-@prefix address: <https://schema.org/streetAddress> .
+LOCATED_IN = TypeDef(
+    reference=Reference(prefix="RO", identifier="0001025", name="located in"),
+)
+HAS_LATITUDE = TypeDef(
+    reference=Reference(prefix="wgs84", identifier="latititude"),
+    range=Reference(prefix="xsd", identifier="decimal"),
+    is_metadata_tag=True,
+)
+# .append_equivalent(Reference(prefix="schema", identifier="latitude")))
 
-<{ONTOLOGY_IRI}> a owl:Ontology ;
-    terms:title "Event Venue Registry" ;
-    terms:description "An ontology representation of the Event Venue Registry" ;
-    terms:license <https://creativecommons.org/publicdomain/zero/1.0/> ;
-    IAO:0000700 venue: ;
-    terms:creator orcid:0000-0003-4423-4370 ;
-    rdfs:comment "Built by https://github.com/cthoyt/event-venue-registry"^^xsd:string .
+HAS_LONGITUDE = TypeDef(
+    reference=Reference(prefix="wgs84", identifier="longitude"),
+    range=Reference(prefix="xsd", identifier="decimal"),
+    is_metadata_tag=True,
+)
+# .append_equivalent(Reference(prefix="schema", identifier="longitude"))
 
-IAO:0000700 a owl:AnnotationProperty;
-    rdfs:label "ontology root"^^xsd:string .
+HAS_ADDRESS = TypeDef(
+    reference=Reference(prefix="schema", identifier="streetAddress"),
+    is_metadata_tag=True,
+)
 
-RO:0001025 a owl:ObjectProperty;
-    rdfs:label "located in"^^xsd:string .
+HAS_HOMEPAGE = TypeDef(
+    reference=Reference(prefix="foaf", identifier="homepage"),
+    is_metadata_tag=True,
+)
 
-rdfs:label a owl:AnnotationProperty;
-    rdfs:label "label"^^xsd:string .
 
-skos:exactMatch a owl:AnnotationProperty;
-    rdfs:label "exact match"^^xsd:string .
+def get_terms() -> list[Term]:
+    """Get PyOBO terms."""
+    from pyobo.struct import CHARLIE_TERM, HUMAN_TERM
 
-geo:lat a owl:AnnotationProperty;
-    rdfs:label "latitude"^^xsd:string ;
-    owl:equivalentProperty schema:latitude ;
-    rdfs:range xsd:decimal .
+    rv: list[Term] = [CONFERENCE_VENUE_TERM, HUMAN_TERM, CHARLIE_TERM, CONFERENCE_CITY_TERM]
+    venues = load_venues()
+    rv.extend(_get_term(t) for t in venues)
 
-geo:long a owl:AnnotationProperty;
-    rdfs:label "longitude"^^xsd:string ;
-    owl:equivalentProperty schema:longitude ;
-    rdfs:range xsd:decimal .
+    cities = {}
+    contributors = {}
+    for venue in venues:
+        if venue.city_geonames not in cities:
+            cities[venue.city_geonames] = Term(
+                reference=Reference(
+                    prefix="geonames", identifier=venue.city_geonames, name=venue.city_label
+                ),
+                type="Instance",
+            ).append_parent(CONFERENCE_CITY_TERM)
 
-address: a owl:AnnotationProperty;
-    rdfs:label "street address"^^xsd:string .
+        if venue.creator:
+            contributors[venue.creator] = Term(
+                reference=Reference(prefix="orcid", identifier=venue.creator), type="Instance"
+            ).append_parent(HUMAN_TERM)
 
-venue: a owl:Class;
-    rdfs:label "conference venue"^^xsd:string .
+    rv.extend(cities.values())
+    rv.extend(contributors.values())
+    return rv
 
-city: a owl:Class;
-    rdfs:label "city"^^xsd:string .
 
-human: a owl:Class;
-    rdfs:label "Homo sapiens"^^xsd:string .
-
-"""
+def _get_term(venue: Venue) -> Term:
+    """Convert a venue into a term."""
+    term = Term(
+        reference=Reference(prefix="evr", identifier=venue.id, name=venue.name),
+        type="Instance",
+    )
+    term.append_parent(CONFERENCE_VENUE_TERM)
+    term.annotate_literal(HAS_ADDRESS, venue.address)
+    term.annotate_decimal(HAS_LONGITUDE, venue.longitude)
+    term.annotate_decimal(HAS_LATITUDE, venue.latitude)
+    term.append_relationship(
+        LOCATED_IN,
+        Reference(prefix="geonames", identifier=venue.city_geonames, name=venue.city_label),
+    )
+    term.append_contributor(Reference(prefix="orcid", identifier=venue.creator))
+    term.append_see_also_uri(venue.google_maps_link)
+    # TODO add OSM to bioregistry! osmw: <https://www.openstreetmap.org/way/> .
+    # TODO add exact match for venue.osm_way
+    if venue.homepage:
+        term.annotate_uri(HAS_HOMEPAGE, venue.homepage)
+    if venue.wikidata:
+        term.append_exact_match(Reference(prefix="wikidata", identifier=venue.wikidata))
+    # TODO if venue.local_name and venue.lang:
+    return term
 
 
 def _get_orcid_name(orcid: str) -> str | None:
@@ -96,64 +124,39 @@ def _get_orcid_name(orcid: str) -> str | None:
     return None
 
 
-def export_ontology(output_path: Path, *, input_path: Path | None = None) -> None:
-    """Export EVR as an ontology."""
-    from pyobo import get_name
-
-    venues = load_venues(path=input_path)
-    with output_path.open("w") as outfile:
-        print(PREAMBLE, file=outfile)
-        city_geonames_ids: set[str] = set()
-        creator_orcids: set[str] = set()
-        for venue in venues:
-            parts = [
-                "a venue:",
-                f'rdfs:label "{venue.name}"@en',
-                f'address: "{venue.address}"',
-                f"RO:0001025 geonames:{venue.city_geonames}",
-                f"geo:lat {venue.latitude}",
-                f"geo:long {venue.longitude}",
-                f"terms:creator orcid:{venue.creator}",
-                f"rdfs:seeAlso <{venue.google_maps_link}>",
-            ]
-            city_geonames_ids.add(venue.city_geonames)
-            creator_orcids.add(venue.creator)
-            if venue.wikidata:
-                parts.append(f"skos:exactMatch wikidata:{venue.wikidata}")
-            if venue.local_name and venue.lang:
-                parts.insert(2, f'rdfs:label "{venue.local_name}"@{venue.lang}')
-            if venue.osm_way:
-                parts.append(f"skos:exactMatch osmw:{venue.osm_way}")
-            if venue.homepage:
-                parts.append(f"foaf:homepage <{venue.homepage}>")
-            outfile.write(f"evr:{venue.id} " + " ;\n    ".join(parts) + " .\n\n")
-
-        for city_geonames_id in tqdm(sorted(city_geonames_ids), unit="city"):
-            city_name = get_name("geonames", city_geonames_id)
-            outfile.write(f'geonames:{city_geonames_id} a city: ; rdfs:label "{city_name}" .\n\n')
-
-        # TODO city to country "located in" link
-
-        for orcid in tqdm(sorted(creator_orcids), unit="person"):
-            person_name = _get_orcid_name(orcid)
-            if person_name:
-                outfile.write(f'orcid:{orcid} a human: ; rdfs:label "{person_name}" .\n\n')
-            else:
-                outfile.write(f"orcid:{orcid} a human: .\n\n")
-
-
 @click.command()
 @click.option("--path", type=Path)
 def main(path: Path | None) -> None:
     """Export EVR as an ontology."""
-    export_ontology(input_path=path, output_path=ONTOLOGY_TTL_PATH)
+    from pyobo.struct import OBOLiteral
+    from pyobo.struct import vocabulary as v
+    from pyobo.struct.struct_utils import Annotation
+
+    ontology = make_ad_hoc_ontology(
+        PREFIX,
+        _name="Event Venue Registry",
+        terms=get_terms(),
+        _typedefs=[LOCATED_IN, HAS_LONGITUDE, HAS_ADDRESS, HAS_LATITUDE, HAS_HOMEPAGE, exact_match],
+        _root_terms=[
+            CONFERENCE_CITY_TERM.reference,
+            CONFERENCE_VENUE_TERM.reference,
+        ],
+        _property_values=[
+            Annotation(
+                v.comment,
+                OBOLiteral.string("Built by https://github.com/cthoyt/event-venue-registry"),
+            ),
+            Annotation(v.has_license, Reference(prefix="spdx", identifier="CCO-1.0")),
+        ],
+    )
+    ontology.write_obo(ONTOLOGY_OBO_PATH)
+    ontology.write_rdf(ONTOLOGY_TTL_PATH)
 
     from bioontologies import robot
 
     robot.convert(ONTOLOGY_TTL_PATH, ONTOLOGY_OWL_PATH)
 
-    # Note: OBO isn't exported since ROBOT/OWLAPI
-    # doesn't support [Instance] stanzas
+    make_site(ontology, HTML_DIRECTORY, manifest=True)
 
 
 if __name__ == "__main__":
